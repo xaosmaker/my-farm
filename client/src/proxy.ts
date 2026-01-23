0d072ce125cb80601c72e1f90ebff6d865b49b26
Drosinakis Drosos <drosinakis.drosos1@gmail.com>
2026-01-23 22:02:49 +0200

    feat: prod headers for nginx and deploy

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -1,12 +1,45 @@
-// middleware.ts
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 import { NextRequest } from "next/server";
 
-export default async function proxy(req: NextRequest) {
-  const intlMiddleware = createMiddleware(routing);
-  const res = intlMiddleware(req);
-  return res;
+const intlMiddleware = createMiddleware(routing);
+
+export function proxy(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    style-src-attr 'self' 'unsafe-inline';
+    style-src-elem 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  const response = intlMiddleware(request);
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  return response;
 }
 
 export const config = {
