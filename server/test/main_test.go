2d8e9c81af8022fe3b2ec100cf5c4e3d6e71f03a
Drosinakis Drosos <drosinakis.drosos1@gmail.com>
2026-01-27 13:53:26 +0200

    add: test for login api

--- server/test/main_test.go
+++ server/test/main_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http/httptest"
@@ -16,74 +15,12 @@ import (
 	"github.com/xaosmaker/server/internal/db"
 )
 
-var testServer *chi.Mux
-var cookie string
+type Cookie = string
+
 var conn *pgxpool.Pool
 var ctx context.Context
-
-func getIdFromBody(s string) (id string) {
-	id = ""
-	for line := range strings.SplitSeq(s, ",") {
-		if strings.Contains(line, "id") {
-			id = strings.Split(line, ":")[1]
-			break
-
-		}
-	}
-
-	return id
-
-}
-
-func createField(cookie string) fieldResponse {
-
-	req := httptest.NewRequest("POST", "/api/fields", strings.NewReader(`{"name":"randomName","fieldLocation":"randomName","areaInMeters":35000}`))
-	req.Header.Set("Cookie", cookie)
-	res := httptest.NewRecorder()
-	testServer.ServeHTTP(res, req)
-	decoder := json.NewDecoder(res.Body)
-	createdField := fieldResponse{}
-	decoder.Decode(&createdField)
-	if res.Code != 201 {
-		log.Fatalf("Cannot create field to delete later")
-	}
-	return createdField
-}
-
-func setLandUnit(str, cookie string) error {
-	req := httptest.NewRequest("POST", "/api/settings", strings.NewReader(fmt.Sprintf(`{"landUnit":"%s"}`, str)))
-	req.Header.Set("Cookie", cookie)
-	res := httptest.NewRecorder()
-	testServer.ServeHTTP(res, req)
-	if res.Code != 204 {
-		return fmt.Errorf("Cant set the settings to: %s, body: %s, code: %d", str, res.Body, res.Code)
-	}
-	return nil
-}
-func loginUserCookie(email, password string) string {
-
-	body := fmt.Sprintf(`{"email":"%v", "password":"%v"}`, email, password)
-	req := httptest.NewRequest("POST", "/api/users/login", strings.NewReader(body))
-	res := httptest.NewRecorder()
-	testServer.ServeHTTP(res, req)
-	c := res.Header().Get("Set-Cookie")
-	if c == "" {
-		log.Fatal("no cookie found")
-	}
-	return c
-}
-func deleteField(fieldId any, cookie string) bool {
-
-	req := httptest.NewRequest("DELETE", fmt.Sprintf("/api/fields/%v", fieldId), nil)
-	req.Header.Set("Cookie", cookie)
-	res := httptest.NewRecorder()
-	testServer.ServeHTTP(res, req)
-	return res.Code == 204
-}
-
-func toPtr[T any](val T) *T {
-	return &val
-}
+var testServer *chi.Mux
+var cookie Cookie
 
 func TestMain(m *testing.M) {
 	ctx = context.Background()
@@ -96,4 +33,17 @@ func TestMain(m *testing.M) {
 	conn.Close()
 
 	os.Exit(code)
+}
+
+func loginUserCookie(email, password string) Cookie {
+
+	body := fmt.Sprintf(`{"email":"%v", "password":"%v"}`, email, password)
+	req := httptest.NewRequest("POST", "/api/users/login", strings.NewReader(body))
+	res := httptest.NewRecorder()
+	testServer.ServeHTTP(res, req)
+	c := res.Header().Get("Set-Cookie")
+	if c == "" {
+		log.Fatal("no cookie found")
+	}
+	return c
 }
