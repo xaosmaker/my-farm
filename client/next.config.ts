c61387eda9f2efd7836c7c3c53b6e8368f5fe4ff
Drosinakis Drosos <drosinakis.drosos1@gmail.com>
2026-01-27 22:59:24 +0200

    configure jest

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const test = process.env.NODE_ENV === "test";
+const pack = test ? { transpilePackages: ["next-intl", "use-intl"] } : {};
 const nextConfig: NextConfig = {
+  ...pack,
   /* config options here */
   // serverExternalPackages: ["pdf-parse"],
   experimental: {
