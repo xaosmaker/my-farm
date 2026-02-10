c2f5a0f5c1714a60062143f8ed35464f124cca7d
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-10 10:29:32 +0200

    chore: add i18n micro

--- nux-client/nuxt.config.ts
+++ nux-client/nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/hints'
-  ]
-})
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/hints",
+    "nuxt-i18n-micro",
+  ],
+  i18n: {
+    locales: [
+      { code: "en", iso: "en-US", dir: "ltr" },
+      { code: "el", iso: "el-GR", dir: "ltr" },
+    ],
+    defaultLocale: "en",
+    fallbackLocale: "en",
+    strategy: "prefix_except_default",
+    redirects: true,
+    autoDetectLanguage: true,
+    localeCookie: "my-farm-lang",
+  },
+});
