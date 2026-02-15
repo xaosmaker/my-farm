7aed1d624048b873572d362d6089ad260fd2d67a
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-15 06:26:24 +0200

    error with zod need fix

--- next-client/src/features/auth/forms/LoginForm.tsx
+++ next-client/src/features/auth/forms/LoginForm.tsx
@@ -1,4 +1,63 @@
 "use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Controller, useForm } from "react-hook-form";
+import { loginSchema, type LoginSchema } from "../schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 export default function LoginForm() {
-  return <div>Login Form</div>;
+  const { control } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login Desc</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="name" className="ml-2">
+                    label
+                  </FieldLabel>
+                  <Input
+                    id="name"
+                    {...field}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
