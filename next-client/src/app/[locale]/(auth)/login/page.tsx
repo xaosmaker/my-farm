3da7c04ee5f397b35d17b664d1fc5feec702d591
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-15 17:46:28 +0200

    fix: fix metadata for login page

--- next-client/src/app/[locale]/(auth)/login/page.tsx
+++ next-client/src/app/[locale]/(auth)/login/page.tsx
@@ -1,4 +1,14 @@
 import LoginForm from "@/features/auth/forms/LoginForm";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const t = await getTranslations("Global.metaData");
+
+  return {
+    title: t("login"),
+  };
+};
 
 export default function LoginPage() {
   return <LoginForm />;
