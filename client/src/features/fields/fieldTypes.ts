ae5587e3ce6dc09e5bb2d1d07c7330c30f4c82ba
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-20 02:53:32 +0200

    feat: add field table closes #10

---
+++ client/src/features/fields/fieldTypes.ts
@@ -0,0 +1,23 @@
+import { DateSTR } from "@/types/genetalTypes";
+
+interface BaseField {
+  id: number;
+  name: string;
+  epsg2100Boundary: undefined | Array<[number, number]>;
+  epsg4326Boundary: undefined | Array<[number, number]>;
+  mapLocation: undefined | Array<[number, number]>;
+  fieldLocation: string;
+  areaInMeters: number;
+  createdAt: DateSTR;
+  updatedAt: DateSTR;
+}
+
+export interface FieldResponse extends BaseField {
+  isOwned: boolean;
+  landUnit: "m2" | "hectares" | "stremata";
+}
+
+export interface Field extends BaseField {
+  isOwned: string;
+  landUnit: string;
+}
