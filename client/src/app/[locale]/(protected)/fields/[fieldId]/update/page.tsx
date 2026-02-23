36ef36853641b7b479385e4eb635ae2efc705491
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-23 02:13:49 +0200

    fix: fix the proteched routes to have auth

--- client/src/app/[locale]/(protected)/fields/[fieldId]/update/page.tsx
+++ client/src/app/[locale]/(protected)/fields/[fieldId]/update/page.tsx
@@ -1,12 +1,14 @@
 import { getField } from "@/features/fields/fieldFetchers";
 import CreateFieldForm from "@/features/fields/forms/CreateFieldForm";
 import { getSettings } from "@/features/settings/settingsFetchers";
+import { getAuth } from "@/lib/getAuth";
 
 export default async function UpdateFieldPage({
   params,
 }: {
   params: Promise<{ fieldId: string }>;
 }) {
+  await getAuth();
   const { fieldId } = await params;
   const userSettings = await getSettings();
   const field = await getField(fieldId);
--- client/src/app/[locale]/(protected)/fields/create/page.tsx
+++ client/src/app/[locale]/(protected)/fields/create/page.tsx
@@ -1,7 +1,9 @@
 import CreateFieldForm from "@/features/fields/forms/CreateFieldForm";
 import { getSettings } from "@/features/settings/settingsFetchers";
+import { getAuth } from "@/lib/getAuth";
 
 export default async function CreateFieldPage() {
+  await getAuth();
   const userSettings = await getSettings();
   return <CreateFieldForm userSettings={userSettings} />;
 }
