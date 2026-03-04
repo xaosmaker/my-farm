7883c52bc7f2fbea6fec299d2a40532534a1b719
xaosmaker <drosinakis.drosos1@gmail.com>
2026-03-04 05:06:57 +0200

    feat: add csp to client

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -4,11 +4,53 @@ import type { NextRequest } from "next/server";
 
 const handleI18nRouting = createMiddleware(routing);
 
-export async function proxy(req: NextRequest) {
-  const res = handleI18nRouting(req);
+// export async function proxy(req: NextRequest) {
+//   const res = handleI18nRouting(req);
+//
+//   return res;
+// }
 
-  return res;
+export function proxy(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    style-src-attr 'self' 'unsafe-inline';
+    style-src-elem 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  const response = handleI18nRouting(request);
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  return response;
 }
+
+// export const config = {
+//   matcher: ["/((?!_next|api).*)"],
+// };
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
